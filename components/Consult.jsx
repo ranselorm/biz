e2bbdc6fe9ed68d9f7a6b8d42053ab576ea8c370
Selorm <ranselorm@outlook.com>
2023-10-02 14:14:44 +0100

    Make Services responsive

--- components/Consult.jsx
+++ components/Consult.jsx
@@ -3,24 +3,10 @@ import { BiPhoneCall } from "react-icons/bi";
 
 const Consult = () => {
   return (
-    <div className="-mt-[80px] z-50">
-      <div className="px-[100px] max-w-[1100px] w-full h-[150px] bg-[#6CB4EE] mx-auto rounded-3xl flex  items-center">
-        {/* <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <div className="w-[65px] h-[65px] bg-transparent border border-white rounded-full flex items-center justify-center">
-              <BiPhoneCall className="text-white text-4xl font-black animate-pulse" />
-            </div>
-            <div>
-              <p>Text1</p>
-              <p>Text2</p>
-            </div>
-          </div>
-          <div>Line</div>
-          <div>Para</div>
-          <div>Button</div>
-        </div> */}
-        <div className="flex justify-between items-center w-full">
-          <div className="flex gap-x-6">
+    <div className="-mt-[80px]">
+      <div className="lg:px-[100px] px-[50px] lg:max-w-[1100px] w-full lg:h-[150px] h-[400px] bg-[#6CB4EE] mx-auto rounded-3xl flex items-center">
+        <div className="flex flex-col gap-y-10 lg:gap-y-0 lg:flex-row justify-between items-center w-full">
+          <div className="flex gap-x-6 mr-8">
             <div className="w-[60px] h-[60px] bg-transparent border border-white rounded-full flex items-center justify-center">
               <BiPhoneCall className="text-white text-4xl font-black animate-pulse" />
             </div>
@@ -29,12 +15,12 @@ const Consult = () => {
               <p className="text-4xl font-bold">+23355444000</p>
             </div>
           </div>
-          <div className="w-[2px] h-[100px] bg-red-700" />
-          <h4 className="text-sm max-w-2xl w-full">
+          <div className="w-[2px] h-[100px] bg-red-700 hidden lg:flex" />
+          <h4 className="text-sm mx-8 w-full font-semibold text-center lg:text-left">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
             iste doloremque velit. Quasi totam earum
           </h4>
-          <button className="bg-white px-[50px] py-3 rounded-full uppercase">
+          <button className="bg-white px-[50px] py-3 rounded-full w-full uppercase">
             Appointment
           </button>
         </div>
